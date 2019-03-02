4d5b7546bf64564d0319b7358f7853f87f92004e
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-03-02 14:18:48 -0300

    Início do exercício - perguntas básicas

---
+++ exercicio2.php
@@ -0,0 +1,13 @@
+<?php
+
+print "Quantidade de dias: ";
+$diasquantidade = (int) fgets(STDIN);
+
+print "Quantidade de horas: ";
+$horasquantidade = (int) fgets(STDIN);
+
+print "Quantidade de minutos: ";
+$minutosquantidade = (int) fgets(STDIN);
+
+print "Quantidade de segundos: ";
+$segundosquantidade = (float) fgets(STDIN);
