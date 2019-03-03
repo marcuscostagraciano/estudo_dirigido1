5bde255ef43ea4660049c70c9863d7c1679c1803
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-03-03 17:49:32 -0300

    Término do exercício

--- exercicio2.php
+++ exercicio2.php
@@ -11,3 +11,10 @@ $minutosquantidade = (int) fgets(STDIN);
 
 print "Quantidade de segundos: ";
 $segundosquantidade = (float) fgets(STDIN);
+
+$diastosegundos = $diasquantidade*24*60*60;
+$horastosegundos = $horasquantidade*60*60;
+$minutostosegundos = $minutosquantidade*60;
+  $segundostotais = $diastosegundos+$horastosegundos+$minutostosegundos+$segundosquantidade;
+  
+print "Segundos totais: $segundostotais";
