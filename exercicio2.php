073f537af22f1e7458340d45f86165f5b7ca6923
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-03-06 19:49:56 -0300

    Mudanças básicas e desnecessárias

--- exercicio2.php
+++ exercicio2.php
@@ -1,20 +1,15 @@
 <?php
-
-print " Quantidade de dias: ";
+print "Quantidade de dias: ";
 $diasquantidade = (int) fgets(STDIN);
-
-print "\n Quantidade de horas: ";
+print "Quantidade de horas: ";
 $horasquantidade = (int) fgets(STDIN);
-
-print "\n Quantidade de minutos: ";
+print "Quantidade de minutos: ";
 $minutosquantidade = (int) fgets(STDIN);
-
-print "\n Quantidade de segundos: ";
+print "Quantidade de segundos: ";
 $segundosquantidade = (float) fgets(STDIN);
-
 $diastosegundos = $diasquantidade*24*60*60;
 $horastosegundos = $horasquantidade*60*60;
 $minutostosegundos = $minutosquantidade*60;
   $segundostotais = $diastosegundos+$horastosegundos+$minutostosegundos+$segundosquantidade;
   
-print "\n Segundos totais: $segundostotais";
+print " Segundos totais: $segundostotais";
